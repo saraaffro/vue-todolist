8ebc28d28aa5b264f68f0b46680d0eec3948541b
saraaffro <saraaffro@gmail.com>
2023-12-12 13:15:57 +0100

    feat: output tasks

--- js/script.js
+++ js/script.js
@@ -3,7 +3,20 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
         }
     },
     methods: {
